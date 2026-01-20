f5d59b6661cb73ccc2262dff1fec85ec00250f6e
AbmSourav <keramotul.islam@gmail.com>
2026-01-20 13:11:29 +0600

    plugin init

---
+++ search.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package wp_seeder
+ * @version 1.0.0
+ */
+
+/*
+Plugin Name: Deep Search
+Plugin URI:
+Description: Search plugin for WordPress
+Author: Keramot UL Islam
+Version: 1.0.0
+Author URI: https://abmsourav.com
+*/
+
+// Do not load directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+require __DIR__ . '/vendor/autoload.php';
